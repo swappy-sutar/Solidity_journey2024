44ccbb36a9dd4255868cff997f2267afbd7ced58
swap-sutar <sutarswapnil322@gmail.com>
2024-05-22 10:46:16 +0530

    use of modifers keyword

--- basic/4_modifier.sol
+++ basic/4_modifier.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
-        //function modifiers
-        //Modifiers are code that can be run before and after the function call.
-        
-        //it's used for 3 reason's 
-    //     1. Restriction.
-    //     2. Validate Inputs.
-    //     3. Guard against reentrancy hack.
+            //function modifiers
+            //Modifiers are code that can be run before and after the function call.
+            
+            //it's used for 3 reason's 
+        //     1. Restriction.
+        //     2. Validate Inputs.
+        //     3. Guard against reentrancy hack.
 
     contract modifierKeyword{
 
@@ -29,10 +29,24 @@ pragma solidity 0.8.0;
             _;
         }
 
-        function changeOwner(address newOwner)public onlyOwner validateData(newOwner) returns (bool){
+        function changeOwner(address newOwner)public onlyOwner validateData(newOwner){
             owner = newOwner;
-            return locked = true;
         }
          
+        modifier noReentrancy(){
+            require(!locked,"NO Reentrancy");
+            locked = true;
+            _;
+            locked = false;
+        }
+
+        function decrement(uint i) public noReentrancy{
+            data -= 1;
+
+            if(data > 1){
+            decrement(i - 1);
+            }
+        }
+
     }
 
